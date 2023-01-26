af01e66349979a40312a70900d91640ae626500f
Oleksii Savchuk <savchukoleksii@gmail.com>
2023-01-26 22:05:24 +0200

    Update lib with empty template property

--- src/index.js
+++ src/index.js
@@ -1,27 +1,83 @@
 const regex = /(?<template>((?<directory>[a-zA-Z0-9_-]+)\/)?(?<name>[a-zA-Z0-9_-]+)\.?(?<suffix>[a-zA-Z0-9_-]+)?)(.+)?/;
 
-const themeTemplateMeta = document.querySelector('[property="theme:template"]');
-if(!themeTemplateMeta) {
-    throw Error("template meta tag does not exist")
-}
-
-const templateString    = themeTemplateMeta.content;
-const parsed            = regex.exec(templateString).groups;
-
 export function templateEquals(template) {
-    return parsed.template === template;
+    const themeTemplateMeta = document.querySelector('[property="theme:template"]');
+    if(!themeTemplateMeta) {
+        return true;
+    }
+
+    const templateString = themeTemplateMeta.content;
+    if (!templateString) {
+        return true;
+    }
+
+    try {
+        const parsed = regex.exec(templateString);
+
+        return parsed.template === template;
+    } catch (error) {}
+
+    return true;
 }
 
 export function templateNameEquals(templateName) {
-    return parsed.name === templateName;
+    const themeTemplateMeta = document.querySelector('[property="theme:template"]');
+    if(!themeTemplateMeta) {
+        return true;
+    }
+
+    const templateString = themeTemplateMeta.content;
+    if (!templateString) {
+        return true;
+    }
+
+    try {
+        const parsed = regex.exec(templateString);
+
+        return parsed.name === templateName;
+    } catch (error) {}
+
+    return true;
 }
 
 export function templateSuffixEquals(templateSuffix) {
-    return parsed.suffix === templateSuffix;
+    const themeTemplateMeta = document.querySelector('[property="theme:template"]');
+    if(!themeTemplateMeta) {
+        return true;
+    }
+
+    const templateString = themeTemplateMeta.content;
+    if (!templateString) {
+        return true;
+    }
+
+    try {
+        const parsed = regex.exec(templateString);
+
+        return parsed.suffix === templateSuffix;
+    } catch (error) {}
+
+    return true;
 }
 
 export function templateDirectoryEquals(templateDirectory) {
-    return parsed.directory === templateDirectory;
+    const themeTemplateMeta = document.querySelector('[property="theme:template"]');
+    if(!themeTemplateMeta) {
+        return true;
+    }
+
+    const templateString = themeTemplateMeta.content;
+    if (!templateString) {
+        return true;
+    }
+
+    try {
+        const parsed = regex.exec(templateString);
+
+        return parsed.directory === templateDirectory;
+    } catch (error) {}
+
+    return true;
 }
 
 export default {
